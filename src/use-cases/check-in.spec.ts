9c23804542031ae283a832da097fb52d77980679
Gabriel Magreti <gabrielmagreti@gmail.com>
2023-07-09 13:03:51 -0300

    test: updating tests

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -20,8 +20,8 @@ describe('Check-in Use Case', () => {
       title: 'JavaScript Gym',
       description: '',
       phone: '',
-      latitude: new Decimal(0),
-      longitude: new Decimal(0),
+      latitude: new Decimal(-27.2092052),
+      longitude: new Decimal(-49.6401091),
     })
     vi.useFakeTimers()
   })
@@ -32,8 +32,8 @@ describe('Check-in Use Case', () => {
 
   it('should to register', async () => {
     const { checkIn } = await sut.execute({
-      gymId: 'gym-id',
-      userId: 'gym-id',
+      gymId: 'gym-01',
+      userId: 'gym-01',
       userLatitude: -27.2092052,
       userLongitude: -49.6401091,
     })
@@ -81,5 +81,25 @@ describe('Check-in Use Case', () => {
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-27.0747279),
+      longitude: new Decimal(-49.4889672),
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: -27.2092052,
+        userLongitude: -49.6401091,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
